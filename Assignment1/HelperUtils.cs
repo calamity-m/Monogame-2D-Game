fe999f26c987a0bc7241b58c1e59b94cce5f005f
monteno-m <markmonteno@gmail.com>
2018-04-19 03:45:12 +1000

    Slight rename and fixed referencing
    removed storage referencing for xna (outdated monogame call?) and added a folder with published build

--- Assignment1/HelperUtils.cs
+++ Assignment1/HelperUtils.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
